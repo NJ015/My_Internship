d441c4497b722d03ddfee67ed549021be71b2588
Nour Jalloul <146312848+NJ015@users.noreply.github.com>
2024-07-26 15:21:35 +0300

    Update dbconfig.js

--- Robot_freinds/app/config/dbconfig.js
+++ Robot_freinds/app/config/dbconfig.js
@@ -1,7 +1,7 @@
-// export default {
-//     url: 'mongodb://localhost:27017/RobotFreinds'
-// }
-
 export default {
-    url: process.env.MONGODB_URL || 'mongodb+srv://nourj2005:mJZCcIHn8Vt0n4Or@robot-friends-cluster.kd6dkja.mongodb.net/RobotFreinds?retryWrites=true&w=majority'
-};
+    url: 'mongodb://localhost:27017/RobotFreinds'
+}
+
+// export default {
+//     url: process.env.MONGODB_URL || 'mongodb+srv://nourj2005:mJZCcIHn8Vt0n4Or@robot-friends-cluster.kd6dkja.mongodb.net/RobotFreinds?retryWrites=true&w=majority'
+// };
