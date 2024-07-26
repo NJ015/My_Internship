fae909114147607c2bb37974276f7c2c010510e2
Nour Jalloul <146312848+NJ015@users.noreply.github.com>
2024-07-26 14:54:50 +0300

    updated the db to atlas

--- Robot_freinds/app/config/dbconfig.js
+++ Robot_freinds/app/config/dbconfig.js
@@ -1,3 +1,7 @@
+// export default {
+//     url: 'mongodb://localhost:27017/RobotFreinds'
+// }
+
 export default {
-    url: 'mongodb://localhost:27017/RobotFreinds'
-}
+    url: process.env.MONGODB_URL || 'mongodb+srv://nourj2005:mJZCcIHn8Vt0n4Or@robot-friends-cluster.kd6dkja.mongodb.net/RobotFreinds?retryWrites=true&w=majority'
+};
